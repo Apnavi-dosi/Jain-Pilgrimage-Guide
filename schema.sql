cbb0c437afc9c14b3a117b3e12c8f6ea02296a42
Apnavi-dosi <jainapnavi@gmail.com>
2026-06-14 17:10:12 +0530

    Add database schema

---
+++ schema.sql
@@ -0,0 +1,36 @@
+CREATE TABLE pilgrimages (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  tirthankar_name VARCHAR(100),
+  tirthankar_number INT,
+  tirthankar_symbol VRACHAR(50),
+  temple_name VRACHAR(255),
+  history_importance TEXT,
+  why_it_isfamous TEXT,
+  location VARCHAR(255),
+  address TEXT,
+  how_to_reach TEXT,
+  facilities_available TEXT,
+  nearby_jain_dharmshala TEXT,
+  nearby_stay TEXT
+);
+
+CREATE TABLE nearby_historical_places (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  pilgrimage_id INT,
+  place_name VRACHAR(255),
+  location VRACHAR(255),
+  distance VARCHAR(50),
+  description TEXT,
+  FOREIGN KEY (pilgrimage_id) REFERENCES pilgrimages(id)
+);
+
+CREATE TABLE nearby_jain_temples (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  pilgrimage_id INT,
+  temple_name VARCHAR(255),
+  location VARCHAR(255),
+  distance VARCHAR(50),
+  sect VARCHAR(50),
+  description TEXT,
+  FOREIGN KEY (pilgrimages_id) REFERENCES pilgrimages(id)
+);
